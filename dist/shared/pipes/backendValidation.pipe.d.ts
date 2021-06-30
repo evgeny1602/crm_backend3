deb68a927b85d527d1f0bdb64a61e38656d931c0
Евгений Лобанов <evgeny1602@gmail.com>
2021-06-30 15:36:17 +0700

    Delete dist directory

--- dist/shared/pipes/backendValidation.pipe.d.ts
+++
@@ -1,5 +0,0 @@
-import { ArgumentMetadata, PipeTransform, ValidationError } from '@nestjs/common';
-export declare class BackendValidationPipe implements PipeTransform {
-    transform(value: any, metadata: ArgumentMetadata): Promise<any>;
-    formatErrors(errors: ValidationError[]): {};
-}
