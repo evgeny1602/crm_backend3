6e3cfa04151921ebdb74327ff4a513bd2dae7a7d
Evgeny Lobanov <evgeny1602@gmail.com>
2021-07-01 17:14:07 +0700

    Refactoring

--- src/usergroups/usergroups.service.ts
+++ src/usergroups/usergroups.service.ts
@@ -48,13 +48,29 @@ export class UsergroupsService {
   async findAll(
     query: any
   ): Promise<{ usergroups: Usergroup[], usergroupsCount: number }> {
-    const queryBuilder = getRepository(Usergroup).createQueryBuilder('usergroups');
+    const tableName = 'usergroups';
+    const queryBuilder = getRepository(Usergroup).createQueryBuilder(tableName);
+
+    // Поля таблицы, по которым можно фильтровать.
+    // Если имя поля в URL и в таблице различается,
+    // то используем массив [имя_поля_в_URL, имя_поля_в_таблице]
+    const columnsToSearch = ['name'];
+    for (let column of columnsToSearch) {
+      const c_0: string = Array.isArray(column) ? column[0] : column;
+      const c_1: string = Array.isArray(column) ? column[1] : column;
+      if (query[c_0]) {
+        let filterConfig = {};
+        filterConfig[c_0] = `%${query[c_0]}%`;
+        queryBuilder.andWhere(`${tableName}.${c_1} LIKE :${c_0}`, filterConfig);
+      }
+    }
     const usergroupsCount = await queryBuilder.getCount();
     queryBuilder.limit(query.limit ? query.limit : 10);
     queryBuilder.offset(query.offset ? query.offset : 0);
     const orderType: "ASC" | "DESC" = query.ordertype ? query.ordertype.toUpperCase() : 'ASC';
     const order: string = query.order ? query.order : 'id';
-    queryBuilder.orderBy(`usergroups.${order}`, orderType);
+    queryBuilder.orderBy(`${tableName}.${order}`, orderType);
+    console.log(queryBuilder.getQueryAndParameters())
     const usergroups = await queryBuilder.getMany();
     return { usergroups, usergroupsCount }
   }
